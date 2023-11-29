3a6ede6fbdfe87f2e2fe9bd56bffd7d4e7f37b0e
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-11-29 16:36:44 -0700

    Completed 2/5 Graded Assignments Week2

---
+++ Week#2GradedAssignment/Exercise1GradedAssignment.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int a = 4;
+    int b = 9;
+
+    // Given code:
+
+    cout << (a * 3 + b - 8 / 2) << endl;
+
+    return 0;
+}
