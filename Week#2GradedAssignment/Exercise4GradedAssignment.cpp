f0b5c73c80de21683d5a3dda7eddf473a1f8277d
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-11-29 16:43:47 -0700

    Seeing how my logic was wrong on exercise 4

---
+++ Week#2GradedAssignment/Exercise4GradedAssignment.cpp
@@ -0,0 +1,35 @@
+
+#include <iostream>
+
+using namespace std;
+
+// MARK: Codio Solutions:
+
+int main() {
+
+    double seven = 7.0;
+    double two = 2.0;
+
+    cout << (seven / two) << endl;
+
+    return 0;
+
+}
+
+// MARK: - My solution was correct but process was wrong:
+
+/* int main() {
+
+    float num1 = 7;
+    float num2 = 2;
+
+    float result = (num1 / num2);
+
+    cout << result;
+
+    return 0;
+
+} */
+
+
+
