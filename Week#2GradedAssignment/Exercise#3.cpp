45d1addfcbd1c5be7118560dab3416e161766773
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-11-29 16:32:51 -0700

    first commit

---
+++ Week#2GradedAssignment/Exercise#3.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    string string1 = "Hello ";
+    string string2 = "world";
+
+    string string3 = string1 + string2;
+
+    cout << string3 << endl;
+
+    return 0;
+}
