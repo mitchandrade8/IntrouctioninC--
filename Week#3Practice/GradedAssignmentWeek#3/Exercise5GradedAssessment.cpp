509a567625ec087ccc4ff4de533aaca8681b6d23
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-11-30 15:35:21 -0700

    --Got #5 correct

--- Week#3Practice/GradedAssignmentWeek#3/Exercise5GradedAssessment.cpp
+++ Week#3Practice/GradedAssignmentWeek#3/Exercise5GradedAssessment.cpp
@@ -10,10 +10,16 @@ int main(int argc, char** argv) {
   
   //add code below this line
 
-  
+  if ((x == "a") || (x == "e") || (x == "i") || (x == "o") || (x == "u")) {
+    cout << x + " is a vowel";
+  } else {
+    cout << x + " is not a vowel";
+  }
 
   //add code above this line
   
   return 0;
   
-} 
+}
+
+// Got the same solution as Codio!!!
