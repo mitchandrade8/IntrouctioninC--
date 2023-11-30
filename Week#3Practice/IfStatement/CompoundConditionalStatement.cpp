d67e41ec6ac9d63f27319852861d22b449d575a8
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-11-30 07:20:26 -0700

    2 conditions checked with 'If' statement

---
+++ Week#3Practice/IfStatement/CompoundConditionalStatement.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int num = 16;
+
+    if (num % 2 == 0 && num > 10) {
+        cout << "Even and greater than 10" << endl;
+    }   
+
+    return 0;
+
+}
--- Week#3Practice/IfStatement/IfStatementNotes.cpp
+++ Week#3Practice/IfStatement/IfStatementNotes.cpp
@@ -10,3 +10,11 @@
         // - The keyword 'if'
         // - A boolean expression in parentheses, ().
         // - Curly braces, { }, surrounding all lines of code that will run if the boolean expression is true.
+
+    
+    // If Statements:
+
+        // If statements test to see if a certain condition is true. If yes, then specific commands are run. The simple 'if'
+        // statement does not do anything if the boolean expressions is false.
+    
+    
