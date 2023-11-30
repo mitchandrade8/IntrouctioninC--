3bf8fe85cb15b04b1b632e65c1b4991092ba3101
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-11-30 11:16:27 -0700

    SwitchStatement for the months of the year

---
+++ Week#3Practice/SwitchCaseStatement/SwitchCasevsElseIf.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+
+    return 0;
+
+}
+
+// MARK: - Else If vs Switch Case 
+    /// 'Switch Case' can only check for equality (num == 5), so if you need to check for a range of values
+    /// (num > 50 && num <= 60), use else If instead.
+
