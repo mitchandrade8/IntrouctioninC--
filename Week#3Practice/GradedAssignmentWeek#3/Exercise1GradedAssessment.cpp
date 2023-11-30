21425d53d031d7509412bcfec9dc3822f5756659
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-11-30 12:28:51 -0700

    --Compared my logic to Codio solutions

--- Week#3Practice/GradedAssignmentWeek#3/Exercise1GradedAssessment.cpp
+++ Week#3Practice/GradedAssignmentWeek#3/Exercise1GradedAssessment.cpp
@@ -19,3 +19,31 @@ int main(int argc, char** argv) {
   return 0;
   
 }
+
+// MARK: - Alternative solutions:
+
+    // #1:
+    /* if (((x >= 0) && (x <= 25)) || ((x >= 75) && (x <= 100))) { 
+        cout << to_string(x) + " is between 0 and 25 or 75 and 100"; 
+    } */
+
+
+    // #2:
+    /* if ((x >= 0) && (x <= 25)) { 
+        cout << to_string(x) + " is between 0 and 25 or 75 and 100"; 
+    }
+    if ((x >= 75) && (x <= 100)) {
+        cout << to_string(x) + " is between 0 and 25 or 75 and 100";
+    } */
+
+
+    // #3:
+    /* if ((x >= 0) && (x <= 25)) { 
+        cout << to_string(x) + " is between 0 and 25 or 75 and 100"; 
+    }
+    else if ((x >= 75) && (x <= 100)) {
+        cout << to_string(x) + " is between 0 and 25 or 75 and 100";
+    }
+    else {
+        cout << "";
+    } */
