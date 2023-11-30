03ce84fdf030f090985d2f8702efd6803738e9a0
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-11-29 17:33:25 -0700

    --Finished the Week2 Graded Assignment

---
+++ Week#2GradedAssignment/Exercise2GradedAssignment.cpp
@@ -0,0 +1,18 @@
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int a = 5;
+    int b = 6;
+
+    cout << boolalpha << (a >= b) << endl;
+
+    return 0;
+
+}
+
+// Challenge was to make the console print 'FALSE'
+
