36ef426789303797650028bf19cd903dd9f0b122
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-11-30 07:08:29 -0700

    --C++ must for 'If' statements

---
+++ Week#3Practice/IfStatement/IfStatementNotes.cpp
@@ -0,0 +1,12 @@
+
+
+// MARK: - If Statements:
+
+    /// Conditionals are pieces of code that make a decision about what the program is going to do next. The most 
+    ///     common conditional is the if statement.
+
+
+    // 'If' Statements in C++ must contain the following items:
+        // - The keyword 'if'
+        // - A boolean expression in parentheses, ().
+        // - Curly braces, { }, surrounding all lines of code that will run if the boolean expression is true.
--- Week#3Practice/IfStatement/IfStatementPractice.cpp
+++ Week#3Practice/IfStatement/IfStatementPractice.cpp
@@ -4,6 +4,12 @@ using namespace std;
 
 int main() {
 
+    if (5 > 4) {
+    cout << "I print 1st if true" << endl;
+    cout << "I print 2nd if true" << endl;
+    }   
+    
+    cout << "I will always print" << endl;  
 
     return 0;
     
