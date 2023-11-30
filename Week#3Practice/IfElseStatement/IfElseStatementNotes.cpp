30851efa3692656a2630b42a9d498920c934c71e
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-11-30 07:57:40 -0700

    More if else examples and notes in C++

--- Week#3Practice/IfElseStatement/IfElseStatementNotes.cpp
+++ Week#3Practice/IfElseStatement/IfElseStatementNotes.cpp
@@ -3,4 +3,10 @@
 // MARK: - Overview:
 
     // IF Else Statement Syntax:
-        // 
+        /// The 'If - Else' statement checks to see if a condition is true, and then has specific actions
+        /// that take place. However, it also provides a specific set of actions if the boolean expression 
+        /// is false. Use the else keyword to introduce the code to run when false is evaluated. Note that else is 
+        /// aligned with the if keyword (no identation) and has its own set of curly braces {}. You do not write another 
+        /// boolean expression with else.
+    
+     
---
+++ Week#3Practice/IfElseStatement/IfElseStatementWithNoCurlyBraces.cpp
@@ -0,0 +1,15 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    if (10 % 2 == 0)
+        cout << "10 is even" << endl;
+    else
+        cout << "10 is odd" << endl;
+
+
+    return 0;
+
+}
