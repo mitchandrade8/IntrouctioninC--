5af950bb7b7d473e85b724fdb682c649dd5c57a5
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-11-30 11:40:15 -0700

    More complex examples of all conditionals

--- Week#3Practice/LabReviewWeek#3/IfStatementsOverview.cpp
+++ Week#3Practice/LabReviewWeek#3/IfStatementsOverview.cpp
@@ -5,11 +5,14 @@ using namespace std;
 
 int main() {
 
-    int x = 5;
+    // int x = 5;
+    int x = 20;
 
     if (x < 10) {
         cout << "Less than 10" << endl;
     }
+
+    cout << "And the program continues...";
 
     return 0;
 
