05bd9d9153f9f97bbcc86fc542cee20016f8757f
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-12-01 13:55:48 -0700

    --Start: of While Loops

---
+++ Week#4Practice/ForLoops/ForLoopIteratorVariable.cpp
@@ -0,0 +1,13 @@
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    for (int n=2; n <= 10; n += 2) {
+        cout << n << endl;
+    }
+
+    return 0;
+}
--- Week#4Practice/WhileLoops/whileLoops.cpp
+++ Week#4Practice/WhileLoops/whileLoops.cpp
@@ -5,7 +5,11 @@ using namespace std;
 
 int main() {
 
-
+    int i = 0;
+    while (i < 5) {
+        cout << "Loop# " << i << endl;
+        i++;
+    }
 
     return 0;
     
