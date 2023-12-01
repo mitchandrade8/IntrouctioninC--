6a48d2f7c0b63a33db6abf9e3defc37718852d91
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-11-30 17:44:21 -0700

    --the Keys of making a For Loop

--- Week#4Practice/ForLoops/ForLoops.cpp
+++ Week#4Practice/ForLoops/ForLoops.cpp
@@ -5,11 +5,21 @@ using namespace std;
 
 int main() {
 
+    // Old Inefficient Code: 
+    
+    /* 
     cout << "Hello" << endl;
     cout << "Hello" << endl;
     cout << "Hello" << endl;
     cout << "Hello" << endl;
     cout << "Hello" << endl;
+    */
+
+   // Refined/Refactored code to be efficient:
+
+    for (int i = 0; i < 5; i++) {
+        cout << "Hello" << endl;
+    }
 
     return 0;
 
