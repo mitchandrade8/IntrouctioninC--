a9506a5acbf127bb7457d639c73d106a871ac62f
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-11-30 17:40:01 -0700

    --Start: of For Loops in C++

---
+++ Week#4Practice/ForLoops/ForLoops.cpp
@@ -0,0 +1,16 @@
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout << "Hello" << endl;
+    cout << "Hello" << endl;
+    cout << "Hello" << endl;
+    cout << "Hello" << endl;
+    cout << "Hello" << endl;
+
+    return 0;
+
+}
