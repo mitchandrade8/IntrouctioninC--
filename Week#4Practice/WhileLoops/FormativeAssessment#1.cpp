17608275510439eb94d2d9aece493882bb7baedf
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-12-01 16:38:20 -0700

    More While Loop Examples

---
+++ Week#4Practice/WhileLoops/FormativeAssessment#1.cpp
@@ -0,0 +1,15 @@
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int count = 0;
+
+    while (count < 10) {
+        cout << "C++" << endl;
+        count += 1;
+    }
+
+}
---
+++ Week#4Practice/WhileLoops/FormativeAssessment#2.cpp
@@ -0,0 +1,19 @@
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int total = 0;
+
+    while (true){
+        if (total > 99) {
+            break;
+        }
+    }
+    
+
+    return 0;
+
+}
