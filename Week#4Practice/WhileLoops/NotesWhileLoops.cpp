78bd5ff059cabb166470723c2281da807498f8b4
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-12-01 14:04:42 -0700

    --Undefined conditions are while loops ideal

--- Week#4Practice/WhileLoops/NotesWhileLoops.cpp
+++ Week#4Practice/WhileLoops/NotesWhileLoops.cpp
@@ -3,4 +3,26 @@
 
     // While loops are actually more useful when you are waiting for a certain event to occur!
 
-    
+    // Example:
+
+        // Video Game:
+        // The game should continue until the player loses all of their lives. You do not know how long
+        // this will take, so a while loop is more appropiate. 
+
+    // If you have specific loop parameters, a for loop will be better. 
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int player_lives = 3;
+
+    while (player_lives > 0) {
+        // video game code
+        // goes here
+    }
+
+    return 0;
+
+}
