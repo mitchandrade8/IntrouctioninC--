1699ff694bf45f69ca72ee0462e97ddbc468d965
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-12-01 18:15:06 -0700

    --Start of Nested Loops in C++

---
+++ Week#4Practice/NestedLoops/NotesNestedLoops.cpp
@@ -0,0 +1,8 @@
+
+
+
+    // A 'Nested Loop' is a loop that exists inside of another loop. An advantage of using nested loops is that the loops
+    // can work together to create unique and complex outputs. 
+
+        // Due to their complexity potential, it is rare to see the implementation of more than two nested loops. 
+        // If possible, it is recommended that you refactor your code to reduce this complexity.
