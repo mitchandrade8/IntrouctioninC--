884ba01af04fc02dd144daf98f55ceae881fb1ed
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-12-03 12:25:27 -0700

    --LabReview: For Loop

---
+++ Week#4Practice/NestedLoops/NLFormativeAssessment#2.cpp
@@ -0,0 +1,19 @@
+
+
+#include <iostream>
+using namespace std;
+
+
+int main() {
+
+    for (int i = 0; i < 4; i++) {
+        cout << "&&" << endl;
+        for (int j = 0; j < 3; j++) {
+            cout << "*" << endl;
+        }
+    }
+
+
+    return;
+
+}
