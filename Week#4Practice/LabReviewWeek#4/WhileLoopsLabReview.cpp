ffbbc27e7cdcfa7c2c909c6a134b792eb540d47e
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-12-03 12:37:39 -0700

    --WhileLoops: LabReview

---
+++ Week#4Practice/LabReviewWeek#4/WhileLoopsLabReview.cpp
@@ -0,0 +1,16 @@
+
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    int counter = 0;
+    while (counter < 10) {
+        cout << counter << endl;
+        counter = counter + 1;
+    }
+    cout << "while loop ended" << endl;
+
+    return 0;
+
+}
