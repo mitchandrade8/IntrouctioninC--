4539eee257e1a1b5d6a2fe3a00e9826dedbf1825
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-12-04 11:36:08 -0700

    NestedLoops Cont.

--- Week#4Practice/LabReviewWeek#4/NestedLoopLabReview.cpp
+++ Week#4Practice/LabReviewWeek#4/NestedLoopLabReview.cpp
@@ -5,8 +5,13 @@ using namespace std;
 
 int main() {
 
+    for (int x = 0; x < 3; x++) {
+        cout << "L" << endl;
+        for (int y = 0; y < 3; y++) {
+            cout << "M" << endl;
+        }
+    }
     
-
     return 0;
 
 }
