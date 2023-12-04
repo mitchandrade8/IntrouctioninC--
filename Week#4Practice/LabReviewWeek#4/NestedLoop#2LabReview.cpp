83b772b88c82b3d3d95a41830147c9cc16ea239f
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-12-04 11:41:45 -0700

    NestedLoop#2 Example

---
+++ Week#4Practice/LabReviewWeek#4/NestedLoop#2LabReview.cpp
@@ -0,0 +1,18 @@
+
+
+#include <iostream>
+using namespace std;
+
+
+int main() {
+
+    for (int x = 0; x < 3; x++) {
+        cout << "L";
+        for (int y = 0; y < 3; y++) {
+            cout << "M";
+        }
+    }
+
+    return 0;
+
+}
