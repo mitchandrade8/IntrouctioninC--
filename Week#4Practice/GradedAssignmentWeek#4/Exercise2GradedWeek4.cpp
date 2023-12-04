7b41151f56753c1ff8c0884137783c5547640fb7
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-12-04 12:42:50 -0700

    Failed Exercise #3 first attempt

--- Week#4Practice/GradedAssignmentWeek#4/Exercise2GradedWeek4.cpp
+++ Week#4Practice/GradedAssignmentWeek#4/Exercise2GradedWeek4.cpp
@@ -9,11 +9,8 @@ int main(int argc, char** argv) {
 
     //add code below this line
 
-    int counter = 0;
-
-    while (counter < 10) {
-        cout << "10" << endl;
-        counter += 1;
+    for (int i = 0; i < 10; i++) {
+        cout << x << endl;
     }
     
     //add code above this line
---
+++ Week#4Practice/GradedAssignmentWeek#4/Exercise3GradedWeek4.cpp
@@ -0,0 +1,42 @@
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+  
+  int a = stoi(argv[1]);
+  int b = stoi(argv[2]);
+  
+  if (a > b) {
+    int c = b;
+    b = a;
+    a = c;
+  }
+  
+  //add code below this line
+
+    int sum = 0;
+  
+    for (int iter = a; iter <= b; iter++) {
+        sum += iter;
+    }
+  
+    if (a == b) {
+        sum = a;
+    }
+  
+    cout << sum << endl;
+
+  //add code above this line
+  
+  return 0;
+  
+}
+
+// Or Alternative solution:
+
+/*
+int sum = 0;
+
+
+*/
