cf3765e9f656f00db1a0128c5d457ac2f0635536
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-12-04 13:28:05 -0700

    TurtleGradedAssignmentWeek4

---
+++ Week#4Practice/GradedAssignmentWeek#4/Exercise1GradedWeek4.cpp
@@ -0,0 +1,37 @@
+
+/*
+
+#include <iostream>                    //
+#include "CTurtle.hpp"                 //
+#include "CImg.h"                      //
+using namespace cturtle;               //
+using namespace std;                   //
+/////////////////////////////////////////
+
+int main(int argc, char** argv) {
+  
+  TurtleScreen screen(400, 300); //You may edit the dimensions to fit your window
+  Turtle tina(screen);
+  tina.speed(TS_SLOWEST);
+  
+  //add code below this line
+
+for(int i = 0; i < 3; i++) {
+  tina.forward(100);
+  tina.left(120);
+}
+
+  //add code above this line
+  
+  screen.exitonclick();
+  return 0;
+  
+}
+*/
+
+/*
+    for (int i = 0; i <= 2; i++) {
+        tina.forward(100);
+        tine.left(120);
+    }
+*/
