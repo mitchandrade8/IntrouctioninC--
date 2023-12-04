dff85cf4c8c4591b2c2d1de9b4c3d6ddd4334b66
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-12-04 11:52:44 -0700

    --updated Week4 Exercise2

---
+++ Week#4Practice/GradedAssignmentWeek#4/Exercise2GradedW4.cpp
@@ -0,0 +1,31 @@
+
+
+#include <iostream>
+using namespace std;
+
+int main(int argc, char** argv) {
+
+    string x = argv[1];
+
+    //add code below this line
+
+    int counter = 0;
+
+    while (counter < 10) {
+        cout << "10" << endl;
+        counter += 1;
+    }
+    
+    //add code above this line
+
+    return 0;
+
+}
+
+//Codio Solution:
+
+/* 
+for( int i = 0; i < 10; i++) {
+    cout << x << endl;
+}
+*/
